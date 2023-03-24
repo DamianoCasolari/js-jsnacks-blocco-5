7e1b175b3c76de5443a51da88212246d5ebc6817
DamianoCasolari <damianocasolari@gmail.com>
2023-03-24 12:17:35 +0100

    initial commit

---
+++ jssnack1/main.js
@@ -0,0 +1,71 @@
+// Crea un array di 10 oggetti che rappresentano una zucchina, indicando per ognuna varietà, peso e lunghezza. Calcola quanto pesano tutte le zucchine.
+
+const zuchinis = [
+    {
+        type : rosso,
+        weightKg : 0.5,
+        lengthCm : 21
+
+    },
+    {
+        type : rosso,
+        weightKg : 0.6,
+        lengthCm : 20
+
+    },
+    {
+        type : rosse,
+        weightKg : 0.8,
+        lengthCm : 27
+
+    },
+    {
+        type : rosse,
+        weightKg : 0.9,
+        lengthCm : 30
+
+    },
+    {
+        type : verdi,
+        weightKg : 0.5,
+        lengthCm : 218
+
+    },
+    {
+        type : rosse,
+        weightKg : 0.5,
+        lengthCm : 30
+
+    },
+    {
+        type : rosse,
+        weightKg : 0.5,
+        lengthCm : 15
+
+    },
+    {
+        type : rosse,
+        weightKg : 0.5,
+        lengthCm : 10
+
+    },
+    {
+        type : rosse,
+        weightKg : 0.5,
+        lengthCm : 16
+
+    },
+    {
+        type : rosse,
+        weightKg : 0.7,
+        lengthCm : 25
+
+    },
+] 
+
+let TotalWeightZuchinis = 0;
+zuchinis.forEach((object) => { 
+    TotalWeightZuchinis += object.weightKg;
+})
+
+console.log(TotalWeightZuchinis);
